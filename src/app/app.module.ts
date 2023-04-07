c539a8878559740cd79915d851ac956c1e519c57
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-04-08 02:21:14 +0530

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { InternalservererrorComponent } from './components/internalservererror/internalservererror.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { ManagerComponent } from './components/manager/manager.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { UserComponent } from './components/user/user.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PagenotfoundComponent,
+    InternalservererrorComponent,
+    SignInComponent,
+    SignUpComponent,
+    AdminComponent,
+    ManagerComponent,
+    EmployeeComponent,
+    UserComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
